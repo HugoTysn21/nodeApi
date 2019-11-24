5e133bdb65061c6349f73d9ecf8d8b4479bcbe5b
hugo <hugola1@hotmail.fr>
2019-11-24 18:31:52 +0100

    get api for client

---
+++ public/js/getApi.js
@@ -0,0 +1,15 @@
+$(function () {
+
+    // accede au div du pug (front)
+    let username = $("#username");
+    let message = $("#message");
+
+    // faire un append dans la div en question, apres avoir fetch les data de l'api
+
+    // call api request
+
+    // fetch into var
+
+    // username.append(HTML)
+
+});
